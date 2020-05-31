96e021013a5fdecab2721afe0f4a7f84179eeda6
dev1980 <brham1980@gmail.com>
2020-05-31 22:04:29 +0545

    fizzbuzz problem solved

---
+++ hackerrank/fizzbuzz.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# Write a short program that prints each number from 1 to 100 on a new line.
+# For each multiple of 3, print "Fizz" instead of the number.
+# For each multiple of 5, print "Buzz" instead of the number.
+# For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
+
+(1..100).each do |n|
+  if n % 5 == 0 && n % 3 == 0
+      puts "FizzBuzz"
+  elsif n % 5 == 0 
+      puts "Buzz"
+  elsif n % 3 == 0
+      puts "Fizz"
+  else
+      puts n.to_s
+  end
+end
