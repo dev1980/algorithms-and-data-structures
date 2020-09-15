3f6a31f85155487308a8b60d36a4b217fbc66cfb
dev1980 <brham1980@gmail.com>
2020-09-16 04:22:08 +0545

    solve happy number problem

---
+++ LeetCode/js/202_happy_number.js
@@ -0,0 +1,46 @@
+// 202. Happy Number
+// Easy
+
+// 2400
+
+// 450
+
+// Add to List
+
+// Share
+// Write an algorithm to determine if a number n is "happy".
+
+// A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+
+// Return True if n is a happy number, and False if not.
+
+// Example: 
+
+// Input: 19
+// Output: true
+// Explanation: 
+// 12 + 92 = 82
+// 82 + 22 = 68
+// 62 + 82 = 100
+// 12 + 02 + 02 = 1
+
+var isHappy = function(n) {
+  const seenNumber = new Set();
+  while(n != 1){
+      let currentSum = 0;
+      while(n > 0) {
+          currentSum += (n % 10) * (n % 10)
+          n = Math.floor(n / 10)
+      }
+      if(!seenNumber.has(currentSum)){
+          seenNumber.add(currentSum)
+          n = currentSum
+      } else {
+          return false
+      }
+     
+      }
+   return true
+}
+
+console.log(isHappy(19))
--- js_ads/insertionSert.js
+++ js_ads/insertionSert.js
@@ -1,19 +1,16 @@
-function selectionSort(arr) {
-  var minIdx, temp,
-  len = arr.length;
-  for (var i = 0; i < len; i++) {
-    minIdx = i;
-    for (var j = i+1; j<len; j++) {
-      if(arr[j] < arr[minIdx]) {
-        minIdx = j;
-      }
-    }
-    temp = arr[i];
-    arr[i] = arr[minIdx];
-    arr[minIdx] = temp; 
-    }
-    return arr
-}
+const insertion = arr => {
+  arr.forEach((item, i) => {
+    let num = arr[i];
+    let j;
+
+    for (j = i - 1; j >= 0 && arr[j] > num; j--) {
+      arr[j + 1] = arr[j];
+    };
+    arr[j + 1] = num;
+  });
+
+  return arr;
+};
 
 
-console.log(selectionSort([3, 0, 2, 5, -1, 4, 1]));
+console.log(insertion([3, 0, 2, 5, -1, 4, 1]));
