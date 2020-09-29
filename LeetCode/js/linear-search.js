11f07831a9e9da49e99faf7d52fe9daf5e1a26ea
dev1980 <brham1980@gmail.com>
2020-09-29 22:55:40 +0545

    linear search

---
+++ LeetCode/js/linear-search.js
@@ -0,0 +1,11 @@
+//iterate through the array and find
+function linearSearch(array, n) {
+  for (var i = 0; i < array.length; i++) {
+      if (array[i] == n) {
+          return true;
+      }
+  }
+  return false;
+}
+console.log(linearSearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 6)); // true
+console.log(linearSearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 10)); // false
