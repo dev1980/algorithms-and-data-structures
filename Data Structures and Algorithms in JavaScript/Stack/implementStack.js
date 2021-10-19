f9154b5b21b9c96678266e12b38d718a7eae981d
Brham Dev Mahato <brham1980@gmail.com>
2021-10-19 20:36:30 +0545

    find max object value

--- Data Structures and Algorithms in JavaScript/Stack/implementStack.js
+++ Data Structures and Algorithms in JavaScript/Stack/implementStack.js
@@ -35,11 +35,12 @@ var myStack = new Stack();
 
 myStack.push(1);
 myStack.push(2);
+console.log(myStack)
 console.log(myStack.peek());
 console.log(myStack.pop());
 console.log(myStack.peek());
-myStack.push("racecar");
-console.log(myStack.size());
-console.log(myStack.peek());
-console.log(myStack.pop());
-console.log(myStack.peek());
+// myStack.push("racecar");
+// console.log(myStack.size());
+// console.log(myStack.peek());
+// console.log(myStack.pop());
+// console.log(myStack.peek());
